940c737750a23aad0e3d9d15ab116f18f4f11eae
HAOCHENYE <21724054@zju.edu.cn>
2023-05-24 19:08:40 +0800

    update __init__.py

--- mmlmtools/__init__.py
+++ mmlmtools/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .api import collect_tools, load_tool
+from .api import collect_tools, load_tool, register_custom_tool
 
 collect_tools()
 
-__all__ = ['load_tool', 'collect_tools']
+__all__ = ['load_tool', 'collect_tools', 'register_custom_tool']
