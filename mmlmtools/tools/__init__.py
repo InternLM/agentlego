d2f0a399e8fb883e863c4ef56ba00b4de582c403
Tau-J <674106399@qq.com>
2023-06-01 16:23:25 +0800

    rename

--- mmlmtools/tools/__init__.py
+++ mmlmtools/tools/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .mmagic import Text2ImageTool
-from .mmdet import Text2BoxTool
-from .mmocr import OCRTool
-from .mmpose import HumanBodyPoseTool
-from .mmpretrain import ImageCaptionTool
+from .image_caption import ImageCaptionTool
+from .image_generation import Text2ImageTool
+from .ocr import OCRTool
+from .openset_detection import Text2BoxTool
+from .pose_estimation import HumanBodyPoseTool
 
 __all__ = [
     'ImageCaptionTool', 'Text2BoxTool', 'Text2ImageTool', 'OCRTool',
