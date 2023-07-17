9933ed250a590586b4415b37d0ff08ac41b4015a
Tau-J <674106399@qq.com>
2023-07-17 16:50:28 +0800

    fix bug

--- mmlmtools/tools/__init__.py
+++ mmlmtools/tools/__init__.py
@@ -1,16 +1,22 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+from .anything2image import (Audio2ImageTool, AudioImage2ImageTool,
+                             AudioText2ImageTool, Thermal2ImageTool,)
 from .edge_detection import Image2CannyTool
 from .image_caption import ImageCaptionTool
-from .image_generation import Canny2ImageTool, Seg2ImageTool, Text2ImageTool, Pose2ImageTool
+from .image_generation import (Canny2ImageTool, Pose2ImageTool, Seg2ImageTool,
+                               Text2ImageTool,)
 from .object_detection import ObjectDetectionTool, Text2BoxTool
 from .ocr import OCRTool
 from .pose_estimation import HumanBodyPoseTool
+from .segment_anything import SegmentAnything, SegmentClicked
 from .semseg_tool import SemSegTool
 from .text_qa import TextQuestionAnsweringTool
 
 __all__ = [
     'ImageCaptionTool', 'Text2BoxTool', 'Text2ImageTool', 'OCRTool',
     'HumanBodyPoseTool', 'SemSegTool', 'ObjectDetectionTool',
-    'Image2CannyTool', 'Canny2ImageTool', 'Seg2ImageTool', 'Pose2ImageTool',
-    'TextQuestionAnsweringTool'
+    'Image2CannyTool', 'Canny2ImageTool', 'Seg2ImageTool', 'SegmentAnything',
+    'SegmentClicked', 'TextQuestionAnsweringTool', 'Pose2ImageTool',
+    'Audio2ImageTool', 'Thermal2ImageTool', 'AudioImage2ImageTool',
+    'AudioText2ImageTool'
 ]
