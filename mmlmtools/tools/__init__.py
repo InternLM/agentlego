bc7e84058a296e4c17c96b2cb6cfbfa6748c1523
Tau-J <674106399@qq.com>
2023-07-18 13:35:26 +0800

    exclude imagebind

--- mmlmtools/tools/__init__.py
+++ mmlmtools/tools/__init__.py
@@ -1,10 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .anything2image import (Audio2ImageTool, AudioImage2ImageTool,
-                             AudioText2ImageTool, Thermal2ImageTool,)
+# from .anything2image import (Audio2ImageTool, AudioImage2ImageTool,
+#                              AudioText2ImageTool, Thermal2ImageTool,)
 from .edge_detection import Image2CannyTool
 from .image_caption import ImageCaptionTool
 from .image_generation import (Canny2ImageTool, Pose2ImageTool, Seg2ImageTool,
-                               Text2ImageTool,)
+                               Text2ImageTool)
 from .object_detection import ObjectDetectionTool, Text2BoxTool
 from .ocr import OCRTool
 from .pose_estimation import HumanBodyPoseTool
@@ -13,10 +13,18 @@ from .semseg_tool import SemSegTool
 from .text_qa import TextQuestionAnsweringTool
 
 __all__ = [
-    'ImageCaptionTool', 'Text2BoxTool', 'Text2ImageTool', 'OCRTool',
-    'HumanBodyPoseTool', 'SemSegTool', 'ObjectDetectionTool',
-    'Image2CannyTool', 'Canny2ImageTool', 'Seg2ImageTool', 'SegmentAnything',
-    'SegmentClicked', 'TextQuestionAnsweringTool', 'Pose2ImageTool',
-    'Audio2ImageTool', 'Thermal2ImageTool', 'AudioImage2ImageTool',
-    'AudioText2ImageTool'
+    'ImageCaptionTool',
+    'Text2BoxTool',
+    'Text2ImageTool',
+    'OCRTool',
+    'HumanBodyPoseTool',
+    'SemSegTool',
+    'ObjectDetectionTool',
+    'Image2CannyTool',
+    'Canny2ImageTool',
+    'Seg2ImageTool',
+    'SegmentAnything',
+    'SegmentClicked',
+    'TextQuestionAnsweringTool',
+    'Pose2ImageTool',
 ]
