5df5b78f6c48d9c51c7729f92619e0ae3d561f2b
mzr1996 <mzr1996@163.com>
2023-09-07 17:04:34 +0800

    [Refactor] Refactor image canny related tools.

--- mmlmtools/tools/image_canny/image_to_canny.py
+++ mmlmtools/tools/image_canny/image_to_canny.py
@@ -1,38 +1,34 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Optional
+from typing import Callable, Union
 
 import cv2
 import numpy as np
 
-from mmlmtools.parsers import BaseParser
+from mmlmtools.parsers import DefaultParser
 from mmlmtools.schema import ToolMeta
+from mmlmtools.types import ImageIO
 from ..base import BaseTool
 
 
 class ImageToCanny(BaseTool):
-    DEFAULT_TOOLMETA = dict(
+    DEFAULT_TOOLMETA = ToolMeta(
         name='Edge Detection On Image',
-        model=None,
-        description='This is a useful tool '
-        'when you want to detect the edge of the image.'
-        'input should be a {{{input:image}}}'
-        'output should be a {{{output:image}}}')
+        description='This tool can extract the edge image from an image.',
+        inputs=['image'],
+        outputs=['image'],
+    )
 
-    def __init__(self,
-                 toolmeta: Optional[ToolMeta] = None,
-                 parser: Optional[BaseParser] = None,
-                 remote: bool = False,
-                 device: str = 'cuda'):
-
-        super().__init__(toolmeta, parser, remote, device)
+    def __init__(
+        self,
+        toolmeta: Union[dict, ToolMeta] = DEFAULT_TOOLMETA,
+        parser: Callable = DefaultParser,
+    ):
+        super().__init__(toolmeta=toolmeta, parser=parser)
         self.low_threshold = 100
         self.high_threshold = 200
 
-    def apply(self, inputs: np.ndarray) -> np.ndarray:
-        if self.remote:
-            raise NotImplementedError
-        else:
-            canny = cv2.Canny(inputs, self.low_threshold,
-                              self.high_threshold)[:, :, None]
-            canny = np.concatenate([canny] * 3, axis=2)
-            return canny
+    def apply(self, image: ImageIO) -> ImageIO:
+        canny = cv2.Canny(image.to_array(), self.low_threshold,
+                          self.high_threshold)[:, :, None]
+        canny = np.concatenate([canny] * 3, axis=2)
+        return ImageIO(canny)
