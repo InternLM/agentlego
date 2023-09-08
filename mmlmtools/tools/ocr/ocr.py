8b15674104cad80828b0f953973277a57dec66ad
Tau-J <674106399@qq.com>
2023-09-08 15:09:32 +0800

    refactor 4 tools

--- mmlmtools/tools/ocr/ocr.py
+++ mmlmtools/tools/ocr/ocr.py
@@ -1,73 +1,73 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Optional
+from typing import Callable, Union
 
 import numpy as np
 from PIL import Image
 
-from mmlmtools.parsers import BaseParser
-from mmlmtools.schema import ToolMeta
-from mmlmtools.utils.cache import load_or_build_object
-from ..base import BaseTool
+from mmlmtools.parsers import DefaultParser
+from mmlmtools.types import ImageIO
+from mmlmtools.utils import load_or_build_object, require
+from mmlmtools.utils.schema import ToolMeta
+from ..base_tool import BaseTool
 
 
 class OCR(BaseTool):
-    DEFAULT_TOOLMETA = dict(
+    DEFAULT_TOOLMETA = ToolMeta(
         name='Recognize the Optical Characters On Image',
-        model={
-            'det': 'dbnetpp',
-            'rec': 'svtr-small'
-        },
-        description='This is a useful tool when you want to '
-        'recognize the text from a photo. It takes an {{{input:image}}} as '
-        'the input, and returns a {{{output:text}}} as the output, '
-        'representing the characters or words in the image.')
+        description=('This is a useful tool when you want to recognize the '
+                     'text from a photo. '),
+        inputs=['image'],
+        outputs=['text'],
+    )
 
+    @require(['mmocr>=1.0.1'])
     def __init__(self,
-                 toolmeta: Optional[ToolMeta] = None,
-                 parser: Optional[BaseParser] = None,
-                 remote: bool = False,
-                 device: str = 'cuda'):
-        super().__init__(toolmeta, parser, remote, device)
+                 toolmeta: Union[dict, ToolMeta] = DEFAULT_TOOLMETA,
+                 parser: Callable = DefaultParser,
+                 det_model: str = 'dbnetpp',
+                 rec_model: str = 'svtr-small',
+                 device: str = 'cpu'):
+        super().__init__(toolmeta=toolmeta, parser=parser)
+        self.det_model = det_model
+        self.rec_model = rec_model
+        self.device = device
 
     def setup(self):
         from mmocr.apis import MMOCRInferencer
         self._inferencer = load_or_build_object(
             MMOCRInferencer,
-            det=self.toolmeta.model['det'],
-            rec=self.toolmeta.model['rec'],
+            det=self.det_model,
+            rec=self.rec_model,
             device=self.device)
 
-    def apply(self, image: str) -> str:
-        if self.remote:
-            raise NotImplementedError
-        else:
-            ocr_results = self._inferencer(image, show=False)['predictions'][0]
-            outputs = ocr_results['rec_texts']
+    def apply(self, image: ImageIO) -> str:
+        ocr_results = self._inferencer(image, show=False)['predictions'][0]
+        outputs = ocr_results['rec_texts']
         return outputs
 
 
 class ImageMaskOCR(BaseTool):
-    DEFAULT_TOOLMETA = dict(
+    DEFAULT_TOOLMETA = ToolMeta(
         name='Recognize The Optical Characters On Image With Mask',
-        model={
-            'det': 'dbnetpp',
-            'rec': 'svtr-small'
-        },
-        description='This is a useful tool when you want to '
-        'recognize the characters or words in the masked '
-        'region of the image. '
-        'like: recognize the characters or words in the masked region. '
-        'The input to this tool should be an {{{input:image}}} representing '
-        'the image, and an {{{input:image}}} representing the mask. '
-        'It returns a {{{output:text}}} representing the characters or words '
-        'in the masked region. ')
+        description=('This is a useful tool when you want to recognize the '
+                     'characters or words in the masked region of the image. '
+                     'like: recognize the characters or words in the masked '
+                     'region. '),
+        inputs=['image', 'mask'],
+        outputs=['text'],
+    )
 
+    @require(['mmocr>=1.0.1'])
     def __init__(self,
-                 toolmeta: Optional[ToolMeta] = None,
-                 parser: Optional[BaseParser] = None,
-                 remote: bool = False,
-                 device: str = 'cuda'):
-        super().__init__(toolmeta, parser, remote, device)
+                 toolmeta: Union[dict, ToolMeta] = DEFAULT_TOOLMETA,
+                 parser: Callable = DefaultParser,
+                 det_model: str = 'dbnetpp',
+                 rec_model: str = 'svtr-small',
+                 device: str = 'cpu'):
+        super().__init__(toolmeta=toolmeta, parser=parser)
+        self.det_model = det_model
+        self.rec_model = rec_model
+        self.device = device
 
     def setup(self):
         from mmocr.apis import MMOCRInferencer
@@ -77,17 +77,14 @@ class ImageMaskOCR(BaseTool):
             rec=self.toolmeta.model['rec'],
             device=self.device)
 
-    def apply(self, image_path: str, mask_path: str) -> str:
-        if self.remote:
-            raise NotImplementedError
-        else:
-            image_path = image_path.strip()
-            mask_path = mask_path.strip()
-            mask = Image.open(mask_path).convert('L')
-            mask = np.array(mask, dtype=np.uint8)
-            ocr_results = self._inferencer(image_path, show=False)
-            ocr_results = ocr_results['predictions'][0]
-            seleted_ocr_text = self.get_ocr_by_mask(mask, ocr_results)
+    def apply(self, image: ImageIO, mask: ImageIO) -> str:
+        image = image.strip()
+        mask = mask.strip()
+        mask = Image.open(mask).convert('L')
+        mask = np.array(mask, dtype=np.uint8)
+        ocr_results = self._inferencer(image, show=False)
+        ocr_results = ocr_results['predictions'][0]
+        seleted_ocr_text = self.get_ocr_by_mask(mask, ocr_results)
 
         return seleted_ocr_text
 
@@ -100,12 +97,16 @@ class ImageMaskOCR(BaseTool):
 
         ocr_text_list = []
         num_mask = len(inds[0])
+
         for i in range(num_mask):
             res = self.search((inds[1][i], inds[0][i]), ocr_res)
             if res is not None and len(res) > 0:
                 ocr_text_list.append(res)
-        ocr_text_list = list(dict.fromkeys(ocr_text_list))  # remove duplicates
+
+        # remove duplicates
+        ocr_text_list = list(dict.fromkeys(ocr_text_list))
         ocr_text = '\n'.join(ocr_text_list)
+
         if ocr_text is None or len(ocr_text.strip()) == 0:
             ocr_text = 'No characters in the image'
         else:
@@ -116,6 +117,7 @@ class ImageMaskOCR(BaseTool):
     def search(self, coord, ocr_res):
         det_bboxes = ocr_res.get('det_bboxes', [])
         rec_texts = ocr_res.get('rec_texts', [])
+
         for i, item in det_bboxes:
             left_top = item[:2]
             right_bottom = item[2:]
