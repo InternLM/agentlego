30de823e836cb35befd1b1635ca7105242140d5b
Tau-J <674106399@qq.com>
2023-09-08 15:09:32 +0800

    refactor 4 tools

--- mmlmtools/tools/segmentation/semantic_segmentation.py
+++ mmlmtools/tools/segmentation/semantic_segmentation.py
@@ -1,55 +1,57 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Optional
+from typing import Callable, Union
 
 import mmcv
 
-from mmlmtools.parsers import BaseParser
+from mmlmtools.parsers import DefaultParser
 from mmlmtools.schema import ToolMeta
-from mmlmtools.utils.cache import load_or_build_object
-from ...utils.file import get_new_file_path
+from mmlmtools.types import ImageIO
+from mmlmtools.utils import get_new_file_path, load_or_build_object, require
 from ..base import BaseTool
 
 
 class SemanticSegmentation(BaseTool):
-    DEFAULT_TOOLMETA = dict(
+    DEFAULT_TOOLMETA = ToolMeta(
         name='Segment the Image',
-        model={'model': 'mask2former_r50_8xb2-90k_cityscapes-512x1024'},
-        description='This is a useful tool '
-        'when you only want to segment the picture or segment all '
-        'objects in the picture. like: segment all objects. '
-        'It takes an {{{input:image}}} as the input, and returns '
-        'a {{{output:image}}} as the output, representing the image with '
-        'semantic segmentation results. ')
+        description=(
+            'This is a useful tool when you only want to segment the '
+            'picture or segment all objects in the picture. like: '
+            'segment all objects. '),
+        inputs=['image'],
+        outputs=['image'],
+    )
 
+    @require('mmsegmentation')
     def __init__(self,
-                 toolmeta: Optional[ToolMeta] = None,
-                 parser: Optional[BaseParser] = None,
-                 remote: bool = False,
-                 device: str = 'cuda'):
-        super().__init__(toolmeta, parser, remote, device)
+                 toolmeta: Union[dict, ToolMeta] = DEFAULT_TOOLMETA,
+                 parser: Callable = DefaultParser,
+                 seg_model: str = (
+                     'mask2former_r50_8xb2-90k_cityscapes-512x1024'),
+                 device: str = 'cpu'):
+        super().__init__(toolmeta=toolmeta, parser=parser)
+        self.seg_model = seg_model
+        self.device = device
 
     def setup(self):
         from mmseg.apis import MMSegInferencer
+
         self._inferencer = load_or_build_object(
             MMSegInferencer,
-            model=self.toolmeta.model['model'],
+            model=self.seg_model,
             device=self.device)
 
-    def apply(self, inputs: str) -> str:
-        if self.remote:
-            raise NotImplementedError
-        else:
-            results = self._inferencer(inputs, return_datasamples=True)
-            output_path = get_new_file_path(
-                inputs, func_name='semseg-something')
-            img = mmcv.imread(inputs)
-            img = mmcv.imconvert(img, 'bgr', 'rgb')
-            self._inferencer.visualizer.add_datasample(
-                'results',
-                img,
-                data_sample=results,
-                draw_gt=False,
-                draw_pred=True,
-                show=False,
-                out_file=output_path)
-        return output_path
+    def apply(self, image: ImageIO) -> ImageIO:
+        results = self._inferencer(image, return_datasamples=True)
+        output_path = get_new_file_path(
+            image, func_name='semseg-something')
+        img = mmcv.imread(image)
+        img = mmcv.imconvert(img, 'bgr', 'rgb')
+        self._inferencer.visualizer.add_datasample(
+            'results',
+            img,
+            data_sample=results,
+            draw_gt=False,
+            draw_pred=True,
+            show=False,
+            out_file=output_path)
+        return ImageIO(output_path)
