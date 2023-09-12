7745b2f7c81a920280b175ca29a1614ec22173d8
HAOCHENYE <21724054@zju.edu.cn>
2023-09-12 19:28:36 +0800

    Refactor translations

--- mmlmtools/tools/translation/translation.py
+++ mmlmtools/tools/translation/translation.py
@@ -1,59 +1,54 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Optional
+from typing import Callable, Union
 
-from mmlmtools.parsers import BaseParser
+from mmlmtools.parsers import DefaultParser
 from mmlmtools.schema import ToolMeta
+from mmlmtools.utils import load_or_build_object, require
 from ..base import BaseTool
 
 
 class Translation(BaseTool):
     DEFAULT_TOOLMETA = dict(
         name='Text translation',
-        model={'model_name': 't5-small'},
-        description='This is a useful tool that converts a text from one '
-        'language to another. It takes three arguments as inputs: the '
-        '{{{input:text}}} to be translated, the source language '
-        '{{{input:text}}} and the target language '
-        '{{{input:text}}}. It retures the translated text {{{output:text}}}.',
+        description='This is a useful tool that translates a text from one '
+        'language to another.',
+        inputs=['text', 'text', 'text'],
+        outputs=['text'],
     )
 
     PROMPT = ('translate {source_lang} to {target_lang}: {input}')
 
+    @require('transformers')
     def __init__(self,
-                 toolmeta: Optional[ToolMeta] = None,
-                 parser: Optional[BaseParser] = None,
-                 remote: bool = False,
-                 device: str = 'cpu'):
-        super().__init__(toolmeta, parser, remote, device)
+                 toolmeta: Union[dict, ToolMeta] = DEFAULT_TOOLMETA,
+                 parser: Callable = DefaultParser,
+                 model: str = 't5-small',
+                 device: str = 'cuda'):
+        super().__init__(toolmeta=toolmeta, parser=parser)
 
         self._tokenizer = None
+        self._model_name = model
         self._model = None
+        self._tokenizer = None
+        self.device = device
 
     def setup(self):
-        if self._model is None:
-            try:
-                from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-            except ImportError:
-                raise ImportError('TextTranslationBool tool requires '
-                                  'transformers package, please install it '
-                                  'with `pip install transformers`.')
-            self._tokenizer = AutoTokenizer.from_pretrained(
-                self.toolmeta.model['model_name'])
-            self._model = AutoModelForSeq2SeqLM.from_pretrained(
-                self.toolmeta.model['model_name'])
-
-            self._model.to(self.device)
+        from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+        self._tokenizer = load_or_build_object(
+            AutoTokenizer.from_pretrained,
+            self._model_name,
+        )
+        self._model = load_or_build_object(
+            AutoModelForSeq2SeqLM.from_pretrained,
+            self._model_name,
+        ).to(self.device)
 
     def apply(self, input: str, source_lang: str, target_lang: str) -> str:
-        if self.remote:
-            raise NotImplementedError
-        else:
-            text = self.PROMPT.format(
-                source_lang=source_lang, target_lang=target_lang, input=input)
-            encoded_input = self._tokenizer(
-                text, return_tensors='pt').input_ids.to(self.device)
-            outputs = self._model.generate(encoded_input)
-            output = self._tokenizer.decode(
-                outputs[0], skip_special_tokens=True)
+        text = self.PROMPT.format(
+            source_lang=source_lang, target_lang=target_lang, input=input)
+        encoded_input = self._tokenizer(
+            text, return_tensors='pt').input_ids.to(self.device)
+        outputs = self._model.generate(encoded_input)
+        output = self._tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-            return output
+        return output
--- tests/test_tools/test_translation/test_translation.py
+++ tests/test_tools/test_translation/test_translation.py
@@ -1,26 +1,41 @@
-from unittest import skipIf
+import pytest
 
-from mmengine import is_installed
+from mmlmtools.apis.agents import load_tools_for_hfagent, load_tools_for_lagent
+from mmlmtools.parsers import NaiveParser
+from mmlmtools.testing import setup_tool
 
-from mmlmtools import load_tool
-from mmlmtools.testing import ToolTestCase
-from mmlmtools.tools.parsers import HuggingFaceAgentParser, LangChainParser
+text = 'Legumes share resources with nitrogen-fixing bacteria'
+source_lang = 'English'
+target_lang = 'French'
 
 
-@skipIf(not is_installed('transformers'), reason='requires transformers')
-class TestTranslation(ToolTestCase):
+@pytest.fixture()
+def tool():
+    from mmlmtools.tools import Translation
+    return setup_tool(Translation, device='cuda')
 
-    def test_call(self):
-        text = 'Legumes share resources with nitrogen-fixing bacteria'
-        source_lang = 'English'
-        target_lang = 'French'
 
-        tool = load_tool(
-            'Translation', parser=LangChainParser(), device='cuda')
-        res = tool(text, source_lang, target_lang)
-        assert isinstance(res, str)
+def test_call(tool):
+    tool.set_parser(NaiveParser)
+    res = tool(text, source_lang, target_lang)
+    assert isinstance(res, str)
 
-        tool = load_tool(
-            'Translation', parser=HuggingFaceAgentParser(), device='cuda')
-        res = tool(text, source_lang, target_lang)
-        assert isinstance(res, str)
+
+def test_hf_agent(tool, hf_agent):
+    tools = load_tools_for_hfagent([tool])
+    hf_agent.prepare_for_new_chat()
+    hf_agent._toolbox = {t.name: t for t in tools}
+
+    out = hf_agent.chat(f'Please translate the `{text}` from {source_lang} '
+                        f'to {target_lang}.`')
+    assert out.startswith('Les légumes')
+
+
+def test_lagent(tool, lagent_agent):
+    tools = load_tools_for_lagent([tool])
+    lagent_agent.new_session(tools)
+
+    out = lagent_agent.chat(
+        f'Translate the `{text}` from {source_lang} to {target_lang}')
+    assert out.actions[-1].valid == 1
+    assert out.response.startswith('Les légumes')
