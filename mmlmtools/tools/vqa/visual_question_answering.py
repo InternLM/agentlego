924ccf08ef2b9cf55e25727eacf1d7f2e43a68a2
HAOCHENYE <21724054@zju.edu.cn>
2023-09-13 14:23:46 +0800

    Refactor vqa

--- mmlmtools/tools/vqa/visual_question_answering.py
+++ mmlmtools/tools/vqa/visual_question_answering.py
@@ -1,40 +1,39 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Optional
+from typing import Callable, Union
 
-from mmlmtools.parsers import BaseParser
+from mmlmtools.parsers import DefaultParser
 from mmlmtools.schema import ToolMeta
-from mmlmtools.utils.cache import load_or_build_object
+from mmlmtools.types import ImageIO
+from mmlmtools.utils import load_or_build_object, require
 from ..base import BaseTool
 
 
 class VisualQuestionAnswering(BaseTool):
     DEFAULT_TOOLMETA = dict(
         name='Visual Question Answering',
-        model={'model': 'ofa-base_3rdparty-zeroshot_vqa'},
         description='This is a useful tool '
         'when you want to know some information about the image.'
-        'you can ask questions like "what is the color of the car?"'
-        'The input to this tool should be an {{{input:image}}} and a '
-        '{{{input:text}}}, representing the image and the question.'
-        'It returns a {{{output:text}}} as the output, representing '
-        'the answer to the question. ')
+        'and this tool will return the answer to the question based on '
+        'the image.',
+        inputs=['image', 'text'],
+        outputs=['text'])
 
     def __init__(self,
-                 toolmeta: Optional[ToolMeta] = None,
-                 parser: Optional[BaseParser] = None,
-                 remote: bool = False,
+                 toolmeta: Union[ToolMeta, dict] = DEFAULT_TOOLMETA,
+                 parser: Callable = DefaultParser,
+                 model: str = 'ofa-base_3rdparty-zeroshot_vqa',
                  device: str = 'cuda'):
-        super().__init__(toolmeta, parser, remote, device)
+        super().__init__(toolmeta, parser)
+        self.device = device
+        self.model = model
 
+    @require('mmpretrain')
     def setup(self):
         from mmpretrain.apis import VisualQuestionAnsweringInferencer
         self._inferencer = load_or_build_object(
             VisualQuestionAnsweringInferencer,
-            model=self.toolmeta.model,
+            model=self.model,
             device=self.device)
 
-    def apply(self, image_path: str, text: str) -> str:
-        if self.remote:
-            raise NotImplementedError
-        else:
-            return self._inferencer(image_path, text)[0]['pred_answer']
+    def apply(self, image: ImageIO, text: str) -> str:
+        return self._inferencer(image.to_path(), text)[0]['pred_answer']
