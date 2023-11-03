023228eeaecdbf817c48e6fc06830d7026d41542
mzr1996 <mzr1996@163.com>
2023-11-03 17:08:21 +0800

    Add LangChain demo

---
+++ examples/langchain_example.py
@@ -0,0 +1,39 @@
+from langchain.agents.initialize import initialize_agent
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.llms.openai import OpenAI
+from prompt_toolkit import prompt, ANSI
+
+from agentlego.apis.agents.langchain import load_tools_for_langchain
+
+
+def main():
+    tools = load_tools_for_langchain([
+        'ImageCaption',
+        'Calculator',
+        'SpeechToText',
+    ])
+    llm = OpenAI(temperature=0)
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+
+    agent = initialize_agent(
+        tools,
+        llm,
+        agent='chat-conversational-react-description',
+        verbose=True,
+        memory=memory,
+    )
+
+    while True:
+        try:
+            user = prompt(ANSI('\033[92mUser\033[0m: '))
+        except UnicodeDecodeError:
+            print('UnicodeDecodeError')
+            continue
+        if user == 'exit':
+            exit(0)
+
+        print(agent.invoke({'input': user})['output'])
+
+if __name__ == "__main__":
+    main()
