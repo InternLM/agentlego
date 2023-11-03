21f57cebc809148179ed83f647ed80fd2e2bf909
mzr1996 <mzr1996@163.com>
2023-11-03 16:59:54 +0800

    Update `TextToBbox` tool to return the top-1 object

--- agentlego/tools/object_detection/text_to_bbox.py
+++ agentlego/tools/object_detection/text_to_bbox.py
@@ -1,5 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from typing import Callable, Union
+from typing import Callable, Tuple, Union
 
 from agentlego.parsers import DefaultParser
 from agentlego.schema import ToolMeta
@@ -23,11 +23,11 @@ class TextToBbox(BaseTool):
     """
     DEFAULT_TOOLMETA = ToolMeta(
         name='Detect the Given Object',
-        description=('A useful tool when you only want to show the location '
-                     'of given objects, or detect or find out given objects '
-                     'in the picture. like: locate persons in the picture'),
+        description='The tool can detect the object location according to '
+        'description in English. It will return an image with a bbox of the '
+        'detected object, and the coordinates of bbox.',
         inputs=['image', 'text'],
-        outputs=['image'],
+        outputs=['image', 'text'],
     )
 
     @require('mmdet>=3.1.0')
@@ -35,7 +35,7 @@ class TextToBbox(BaseTool):
                  toolmeta: Union[dict, ToolMeta] = DEFAULT_TOOLMETA,
                  parser: Callable = DefaultParser,
                  model: str = 'glip_atss_swin-t_a_fpn_dyhead_pretrain_obj365',
-                 device: str = 'cpu'):
+                 device: str = 'cuda'):
         super().__init__(toolmeta=toolmeta, parser=parser)
         self.model = model
         self.device = device
@@ -44,9 +44,34 @@ class TextToBbox(BaseTool):
         from mmdet.apis import DetInferencer
         self._inferencer = load_or_build_object(
             DetInferencer, model=self.model, device=self.device)
+        self._visualizer = self._inferencer.visualizer
 
-    def apply(self, image: ImageIO, text: str) -> ImageIO:
-        image = image.to_path()
-        results = self._inferencer(image, texts=text, return_vis=True)
-        output_image = results['visualization'][0]
-        return ImageIO(output_image)
+    def apply(self, image: ImageIO, text: str) -> Tuple[ImageIO, str]:
+        from mmdet.structures import DetDataSample
+
+        results = self._inferencer(
+            image.to_array()[:, :, ::-1],
+            texts=text,
+            return_datasamples=True,
+        )
+        data_sample = results['predictions'][0]
+        preds: DetDataSample = data_sample.pred_instances
+
+        if len(preds) > 0:
+            topk = preds.scores.topk(1).indices
+            preds = preds[topk]
+            score = preds.scores[0].item()
+            bbox = preds.bboxes[0].tolist()
+            pred_str = ('bbox: ({:.0f}, {:.0f}, {:.0f}, {:.0f}), '
+                        'score: {:.2f} (max 1.0)')
+            pred_str = pred_str.format(*bbox, score)
+
+            data_sample.pred_instances = preds
+            self._visualizer.add_datasample(
+                'vis', image.to_array(), data_sample, draw_gt=False)
+            output_image = ImageIO(self._visualizer.get_image())
+        else:
+            pred_str = 'No object found.'
+            output_image = image
+
+        return output_image, pred_str
