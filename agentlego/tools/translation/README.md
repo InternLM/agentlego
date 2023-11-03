275920d4731ffa50b3c48d1fbf3bc57512cae79b
mzr1996 <mzr1996@163.com>
2023-11-03 15:16:42 +0800

    Update the `Translation` tool to use Google Translate API instead of T5 model

--- agentlego/tools/translation/README.md
+++ agentlego/tools/translation/README.md
@@ -1,3 +1,40 @@
 # Translation
 
-## Translation
+## Default Tool Meta
+
+- **name**: Text translation
+- **description**: This tool can translate a text from source language to the target language. The source_lang and target_lang can be one of 'auto' (Detect source language), 'zh-CN' (Chinese), 'en' (English), 'fr' (French), 'de' (German), 'el' (Greek), 'it' (Italian), 'ja' (Japanese), 'ko' (Korean), 'la' (Latin), 'pl' (Polish), 'ru' (Russian), 'es' (Spanish), 'th' (Thai), 'tr' (Turkish).
+- **inputs**: text, text, text
+- **outputs**: text
+
+## Examples
+
+**Use the tool directly (without agent)**
+
+```python
+from agentlego.apis import load_tool
+
+# load tool
+tool = load_tool('Translation')
+
+# apply tool
+res = tool('This is an example sentence.', 'auto', 'zh-CN')
+```
+
+**With Lagent**
+
+```python
+from lagent import ReAct, GPTAPI, ActionExecutor
+from agentlego.apis.agents import load_tools_for_lagent
+
+# load tools and build agent
+# please set `OPENAI_API_KEY` in your environment variable.
+tools = load_tools_for_lagent(tools=['Translation'])
+agent = ReAct(GPTAPI(temperature=0.), action_executor=ActionExecutor(tools))
+
+# agent running with the tool.
+ret = agent.chat(f'Please translate the below text to Chinese: Nothing is true, everything is permitted.')
+for step in ret.inner_steps[1:]:
+    print('------')
+    print(step['content'])
+```
--- agentlego/tools/translation/translation.py
+++ agentlego/tools/translation/translation.py
@@ -1,54 +1,67 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import requests
 from typing import Callable, Union
+from urllib.parse import quote_plus
 
 from agentlego.parsers import DefaultParser
 from agentlego.schema import ToolMeta
-from agentlego.utils import load_or_build_object, require
 from ..base import BaseTool
+
+LANG_CODES = {
+    "auto": "Detect source language",
+    "zh-CN": "Chinese",
+    "en": "English",
+    "fr": "French",
+    "de": "German",
+    "el": "Greek",
+    "it": "Italian",
+    "ja": "Japanese",
+    "ko": "Korean",
+    "la": "Latin",
+    "pl": "Polish",
+    "ru": "Russian",
+    "es": "Spanish",
+    "th": "Thai",
+    "tr": "Turkish",
+}
 
 
 class Translation(BaseTool):
     DEFAULT_TOOLMETA = ToolMeta(
         name='Text translation',
-        description='This is a useful tool that translates a text from one '
-        'language to another.',
+        description='This tool can translate a text from source language to '
+        'the target language. The source_lang and target_lang can be one of ' +
+        ', '.join(f"'{k}' ({v})" for k, v in LANG_CODES.items()) + '.',
         inputs=['text', 'text', 'text'],
         outputs=['text'],
     )
 
     PROMPT = ('translate {source_lang} to {target_lang}: {input}')
 
-    @require('transformers')
     def __init__(self,
                  toolmeta: Union[dict, ToolMeta] = DEFAULT_TOOLMETA,
                  parser: Callable = DefaultParser,
-                 model: str = 't5-small',
-                 device: str = 'cuda'):
+                 backend: str = 'google'):
         super().__init__(toolmeta=toolmeta, parser=parser)
+        if backend == 'google':
+            self._translate = self.google_translate
+        else:
+            raise NotImplementedError(
+                f'The backend {backend} is not available.')
 
-        self._tokenizer = None
-        self._model_name = model
-        self._model = None
-        self._tokenizer = None
-        self.device = device
+    def apply(self, text: str, source_lang: str, target_lang: str) -> str:
+        return self._translate(text, source_lang, target_lang)
 
-    def setup(self):
-        from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
-        self._tokenizer = load_or_build_object(
-            AutoTokenizer.from_pretrained,
-            self._model_name,
-        )
-        self._model = load_or_build_object(
-            AutoModelForSeq2SeqLM.from_pretrained,
-            self._model_name,
-        ).to(self.device)
+    def google_translate(self, text: str, source: str, target: str):
+        text = quote_plus(text)
+        url_tmpl = ('https://translate.googleapis.com/translate_a/'
+                    'single?client=gtx&sl={}&tl={}&dt=at&dt=bd&dt=ex&'
+                    'dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&q={}')
+        response = requests.get(
+            url_tmpl.format(source, target, text), timeout=10).json()
+        try:
+            result = ''.join(x[0] for x in response[0] if x[0] is not None)
+        except:
+            raise ConnectionError('Failed to translate.')
 
-    def apply(self, input: str, source_lang: str, target_lang: str) -> str:
-        text = self.PROMPT.format(
-            source_lang=source_lang, target_lang=target_lang, input=input)
-        encoded_input = self._tokenizer(
-            text, return_tensors='pt').input_ids.to(self.device)
-        outputs = self._model.generate(encoded_input)
-        output = self._tokenizer.decode(outputs[0], skip_special_tokens=True)
-
-        return output
+        return result
